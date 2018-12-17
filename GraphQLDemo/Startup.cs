32cf2c253bf590e5ec2b563e9943fd6085282852
陈鹏 <chenpeng@datamip.cn>
2018-12-17 20:09:40 +0800

    a

---
+++ GraphQLDemo/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Server;
+using GraphQL.Server.Ui.GraphiQL;
+using GraphQL.Server.Ui.Playground;
+using GraphQL.Server.Ui.Voyager;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GraphQLDemo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ChatSchema>();
+
+            // Add GraphQL services and configure options
+            services.AddGraphQL(options =>
+            {
+                options.EnableMetrics = true;
+                options.ExposeExceptions = this.Environment.IsDevelopment();
+            })
+            .AddWebSockets() // Add required services for web socket support
+            .AddDataLoader(); // Add required services for DataLoader support
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            // this is required for websockets support
+            app.UseWebSockets();
+
+            // use websocket middleware for ChatSchema at path /graphql
+            app.UseGraphQLWebSockets<ChatSchema>("/graphql");
+
+            // use HTTP middleware for ChatSchema at path /graphql
+            app.UseGraphQL<ChatSchema>("/graphql");
+
+            // use graphiQL middleware at default url /graphiql
+            app.UseGraphiQLServer(new GraphiQLOptions());
+
+            // use graphql-playground middleware at default url /ui/playground
+            app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
+
+            // use voyager middleware at default url /ui/voyager
+            app.UseGraphQLVoyager(new GraphQLVoyagerOptions());
+        }
+    }
+}
